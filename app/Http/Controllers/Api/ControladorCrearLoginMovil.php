33c421ca94b082a4f9792abb792613a2c254fc1a
Yamil <yarrib76@gmail.com>
2016-04-29 16:06:52 -0300

    Cambios

--- app/Http/Controllers/Api/ControladorCrearLoginMovil.php
+++ app/Http/Controllers/Api/ControladorCrearLoginMovil.php
@@ -16,7 +16,7 @@ class ControladorCrearLoginMovil extends Controller {
         $codigoValidacion = Input::get('codval');
         if ($codigoValidacion == 4015){
             $esValido = $this->validacion($email,$password);
-            if ($esValido[0]['valor'] == "0" and $esValido[1]['valor'] == "1" ){
+            if ($esValido['valor'] == 0 ){
                 $respuesta = $this->crear($email,$password);
                 return Response::json($respuesta);
             }
@@ -26,23 +26,31 @@ class ControladorCrearLoginMovil extends Controller {
     }
 
     // Verifico que el mail y password no vengan vacios
+    // Si los el email y el password tienen datos devuelve valor = 0;
+    // Si falta el email valor = 1;
+    // Si falta la passowrd Valor = 2
+    // Si falta el email y la password valor = 3
     public function validacion($email,$password){
-        $repuesta[0] = ['valor' => "0"];
-        $repuesta[1] = ['valor' => "1"];
+        $repuesta = 0;
+        $cont = 0;
         if (empty($email)){
-            $repuesta[0] = ['valor' => 'Ingresar un Mail'];
+            $cont = $repuesta + 1;
+            $repuesta = [ 'valor' => $cont];
         }
         if (empty($password)){
-            $repuesta[1] = ['valor' => 'Ingresar Una clave'];
+            $cont = $cont + 2;
+            $repuesta = [ 'valor' => $cont];
         }
         return $repuesta;
     }
 
     //Creo el usuario con la password encriptada
+    //Si el usuario ya existia devuelve valor = 4
+    //Si lo crea devuelve un random
     public function crear($email,$password){
         $usuario = User::where("email",$email)->first();
         if ($usuario){
-            return $repuesta[0] = ['valor' => 'El usuario ya Existe'];
+            return $repuesta = [ 'valor' => 4];
         }
         $session_id = $this->crearSessionId();
         User::create([
@@ -50,14 +58,13 @@ class ControladorCrearLoginMovil extends Controller {
             'password' => Hash::make($password),
             'session_id' => $session_id,
         ]);
-        $repuesta[0] = ['valor' => "2"];
-        $repuesta[1] = ['session_id' => $session_id];
+        $repuesta = [ 'valor' => $session_id];
         return $repuesta;
     }
 
     //Creo un # aleatorio para la session del usuario
     public function crearSessionId(){
-        $session_id = rand(1,100000);
+        $session_id = rand(100,100000);
         return $session_id;
     }
 }
