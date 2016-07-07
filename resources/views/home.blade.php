abae4e8b711617e0c6dcbadb794e40ed8097a6a1
Yamil <yarrib76@gmail.com>
2016-07-07 16:24:23 -0300

    Vistas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,6 @@
-@extends('app')
+@extends('layouts.master')
+@section('contenido')
 
-@section('content')
 <div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
-	</div>
 </div>
 @endsection
