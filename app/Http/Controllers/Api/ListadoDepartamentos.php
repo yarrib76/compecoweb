10d088b4fe3b7a5c73a237ab729282ad0d38862b
Yamil <yarrib76@gmail.com>
2016-07-14 16:53:36 -0300

    cambios

--- app/Http/Controllers/Api/ListadoDepartamentos.php
+++ app/Http/Controllers/Api/ListadoDepartamentos.php
@@ -4,14 +4,38 @@ use AlquilerAdmin\Http\Requests;
 use AlquilerAdmin\Http\Controllers\Controller;
 
 use AlquilerAdmin\Models\Departamentos;
+use AlquilerAdmin\User;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Response;
 
 class ListadoDepartamentos extends Controller {
 
 	public function listadoDeptos()
 	{
-		return Response::json(Departamentos::all());
+        $control = $this->validarSession();
+        if ($control){
+            //$departamentos = $this->formateoDatos(Departamentos::all());
+            return Response::json(Departamentos::all()->load('estado'));
+        }
+        return "Invalid session_code ";
 
 	}
+
+    private function validarSession()
+    {
+        $session_id = User::where('email', Input::get('email'))->first()->session_id;
+        if ($session_id == Input::get('session_id')){
+            return true;
+        }
+        return false;
+    }
+
+    private function formateoDatos($datos)
+    {
+        foreach ($datos as $dato){
+
+            dd($dato->load('estado')->estado->tipo);
+        }
+    }
 
 }
