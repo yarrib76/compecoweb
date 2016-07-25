8e7a19ee7541b94065ec57f7c4e42e4a1ff2fc3c
Yamil <yarrib76@gmail.com>
2016-07-25 17:44:22 -0300

    Cambios

--- app/Helper/Sesiones.php
+++ app/Helper/Sesiones.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Input;
 
 class Sesiones
 {
-    public function validarSession($email,$session_id)
+    public function validarSession($email,$inSession_id)
     {
         $session_id = User::where('email', $email)->first()->session_id;
-        if ($session_id == $session_id){
+        if ($session_id == $inSession_id){
             return true;
         }
         return false;
