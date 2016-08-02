b9fad8cd92f3be686f6779652b780df9a7ea10df
Yamil <yarrib76@gmail.com>
2016-08-02 15:11:55 -0300

    Cambios

--- app/Http/Controllers/Api/ApiSavePagoImpuestos.php
+++ app/Http/Controllers/Api/ApiSavePagoImpuestos.php
@@ -3,6 +3,7 @@
 use AlquilerAdmin\Helper\Sesiones;
 use AlquilerAdmin\Http\Requests;
 use AlquilerAdmin\Http\Controllers\Controller;
+use AlquilerAdmin\Models\EstadoImpuestos;
 use AlquilerAdmin\Models\EstadosDeuda;
 use AlquilerAdmin\Models\Impuestos;
 use Illuminate\Support\Facades\Input;
@@ -21,29 +22,82 @@ class ApiSavePagoImpuestos extends Controller{
 
     private function guardarPago()
     {
+        //Ejemplo url http://compraweb.com:8000/api/control_pago_impuestos?email=yarrib76@gmail.com&&session_id=0&&alquiler_id=1&&fecha_pago=2016-09-02&&abl=2&&metrogas=2&&edesur=2&&expensas=2
         $impuestos['abl'] = Input::get('abl');
         $impuestos['metrogas'] = Input::get('metrogas');
         $impuestos['edesur'] = Input::get('edesur');
-        $impuestos['expensas'] = Input::get('espensas');
+        $impuestos['expensas'] = Input::get('expensas');
         $abl_id = Impuestos::where('nombre','abl')->first()->id;
         $metrogas_id = Impuestos::where('nombre','metrogas')->first()->id;
         $edesur_id = Impuestos::where('nombre','edesur')->first()->id;
         $expensas_id = Impuestos::where('nombre','expensas')->first()->id;
         if ($impuestos['abl'] == 1){
-            EstadosDeuda::create([
+            $estadoDeudaId = new EstadosDeuda();
+            EstadoImpuestos::create([
                 'impuesto_id' => $abl_id,
                 'fecha_pago' => Input::get('fecha_pago'),
-                'estado_deuda_id' 
+                'estado_deuda_id'=>$estadoDeudaId->obtengoEstadoId('Al Dia'),
+                'alquiler_id' => Input::get('alquiler_id')
+            ]);
+        } elseif ($impuestos['abl'] == 2){
+            $estadoDeudaId = new EstadosDeuda();
+            EstadoImpuestos::create([
+                'impuesto_id' => $abl_id,
+                'fecha_pago' => Input::get('fecha_pago'),
+                'estado_deuda_id'=>$estadoDeudaId->obtengoEstadoId('Tiene Deuda'),
+                'alquiler_id' => Input::get('alquiler_id')
             ]);
         }
         if ($impuestos['metrogas'] == 1){
-
+            $estadoDeudaId = new EstadosDeuda();
+            EstadoImpuestos::create([
+                'impuesto_id' => $metrogas_id,
+                'fecha_pago' => Input::get('fecha_pago'),
+                'estado_deuda_id'=>$estadoDeudaId->obtengoEstadoId('Al Dia'),
+                'alquiler_id' => Input::get('alquiler_id')
+            ]);
+        } elseif ($impuestos['metrogas'] == 2){
+            $estadoDeudaId = new EstadosDeuda();
+            EstadoImpuestos::create([
+                'impuesto_id' => $metrogas_id,
+                'fecha_pago' => Input::get('fecha_pago'),
+                'estado_deuda_id'=>$estadoDeudaId->obtengoEstadoId('Tiene Deuda'),
+                'alquiler_id' => Input::get('alquiler_id')
+            ]);
         }
         if ($impuestos['edesur'] == 1){
-
+            $estadoDeudaId = new EstadosDeuda();
+            EstadoImpuestos::create([
+                'impuesto_id' => $edesur_id,
+                'fecha_pago' => Input::get('fecha_pago'),
+                'estado_deuda_id'=>$estadoDeudaId->obtengoEstadoId('Al Dia'),
+                'alquiler_id' => Input::get('alquiler_id')
+            ]);
+        } elseif($impuestos['edesur'] == 2){
+            $estadoDeudaId = new EstadosDeuda();
+            EstadoImpuestos::create([
+                'impuesto_id' => $edesur_id,
+                'fecha_pago' => Input::get('fecha_pago'),
+                'estado_deuda_id'=>$estadoDeudaId->obtengoEstadoId('Tiene Deuda'),
+                'alquiler_id' => Input::get('alquiler_id')
+            ]);
         }
-        if ($impuestos['expensas' == 1 ]){
-
+        if ($impuestos['expensas'] == 1){
+            $estadoDeudaId = new EstadosDeuda();
+            EstadoImpuestos::create([
+                'impuesto_id' => $expensas_id,
+                'fecha_pago' => Input::get('fecha_pago'),
+                'estado_deuda_id'=>$estadoDeudaId->obtengoEstadoId('Al Dia'),
+                'alquiler_id' => Input::get('alquiler_id')
+            ]);
+        } elseif($impuestos['expensas'] == 2){
+            $estadoDeudaId = new EstadosDeuda();
+            EstadoImpuestos::create([
+                'impuesto_id' => $expensas_id,
+                'fecha_pago' => Input::get('fecha_pago'),
+                'estado_deuda_id'=>$estadoDeudaId->obtengoEstadoId('Tiene Deuda'),
+                'alquiler_id' => Input::get('alquiler_id')
+            ]);
         }
     }
 }
--- app/Models/EstadosDeuda.php
+++ app/Models/EstadosDeuda.php
@@ -5,4 +5,8 @@ use Illuminate\Database\Eloquent\Model;
 class EstadosDeuda extends Model {
     protected $table = 'estados_deudas';
     protected $fillable = ['tipo'];
+
+    public function obtengoEstadoId($estado){
+        return $estado_id = $this->where('tipo',$estado )->get()->first()->id;
+    }
 }
