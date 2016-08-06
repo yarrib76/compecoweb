dada5184df2ce6a0726e6d49a12c98b0919add09
Yamil <yarrib76@gmail.com>
2016-08-06 08:45:22 -0300

    Cambios

--- app/Http/Controllers/Api/ApiSavePagoAlquiler.php
+++ app/Http/Controllers/Api/ApiSavePagoAlquiler.php
@@ -5,6 +5,7 @@ use AlquilerAdmin\Http\Requests;
 use AlquilerAdmin\Http\Controllers\Controller;
 use AlquilerAdmin\Models\CobroAlquileres;
 use AlquilerAdmin\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
 
 class ApiSavePagoAlquiler extends Controller {
@@ -12,7 +13,10 @@ class ApiSavePagoAlquiler extends Controller {
     public function procesarPago(){
         $sesiones = new Sesiones();
         if ($sesiones->validarSession(Input::get('email'),Input::get('session_id'))){
-            $this->guardarPago();
+            //Si hay un pago parcial, le sumo el nuevo pago. Si no creo un nuevo pago
+            if (!$this->verificoPagoAnterior()){
+                $this->creoPago();
+            }
             // Retorna 1 si se grabo el dato correctamente.
             return ['codigo' => 1];
         }else{
@@ -20,7 +24,7 @@ class ApiSavePagoAlquiler extends Controller {
         }
     }
 
-    public function guardarPago(){
+    public function creoPago(){
         $cobrador_id = User::where('email', Input::get('email'))->get();
         $cobrador_id = $cobrador_id[0]->id;
         CobroAlquileres::create([
@@ -30,5 +34,30 @@ class ApiSavePagoAlquiler extends Controller {
             'observaciones' => Input::get('observaciones'),
             'importe_alquiler' => Input::get('importe_alquiler')
         ]);
+    }
+
+    public function actualizoPago($cobroAlquilerId, $importeActual, $importeAlquiler){
+        db::table('cobro_alquileres')
+            ->where('id', $cobroAlquilerId)
+            ->update([
+                'importe_alquiler' => $importeActual + $importeAlquiler]);
+    }
+    /*
+     * Verifico si hay algun pago parcial.
+     * Si hay, le sumo el nuevo pago
+     */
+    public function verificoPagoAnterior(){
+        //A ESTE QUERY LE FALTA QUE BUESQUE POR AÑO (ejemplo: DESDE EL 01-01-2016, HASTA 31-21-2016)
+        $cobroAlquileres = CobroAlquileres::orderBy('fecha_cobro', 'asc')->
+        where('alquiler_id',Input::get('alquiler_id'))->get();
+        foreach ($cobroAlquileres  as $cobroAlquiler){
+            $mesAnio = date('m-y',strtotime($cobroAlquiler->fecha_cobro));
+            $fechaCobro = date('m-y', strtotime(Input::get('fecha_cobro')));
+            if ($fechaCobro == $mesAnio){
+                $this->actualizoPago($cobroAlquiler->id, $cobroAlquiler->importe_alquiler,
+                    Input::get('importe_alquiler'));
+                return true;
+            }
+        }
     }
 }
--- app/Http/Controllers/Api/ReporteAlquileresPagos.php
+++ app/Http/Controllers/Api/ReporteAlquileresPagos.php
@@ -6,19 +6,77 @@ use AlquilerAdmin\Http\Controllers\Controller;
 use AlquilerAdmin\Models\CobroAlquileres;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Response;
 
 class ReporteAlquileresPagos extends Controller {
 
     public function reportes(){
-        $cobroAlquileres = CobroAlquileres::orderBy('fecha_cobro', 'asc')->get();
-        $this->obtengoMes($cobroAlquileres);
+        $cobroAlquileres = CobroAlquileres::orderBy('fecha_cobro', 'asc')->
+        where('alquiler_id',Input::get('alquiler_id'))->get();
+        $meses = $this->obtengoMeses($cobroAlquileres);
+        return Response::json($meses);
 }
 
-    public function obtengoMes($cobroAlquileres){
+    /*
+     * Obtengo el mes pago, si el Mes = 1 esta pago si no
+     * es Mes = 0
+     */
+    public function obtengoMeses($cobroAlquileres){
+
+        $mesesPagos = $this->initMeses();
         foreach($cobroAlquileres as $cobroAlquiler){
             //dd($cobroAlquiler->fecha_cobro);
-            $mes = Carbon::getWeekendDays($cobroAlquiler->fecha_cobro);
-            dd($mes[0]);
+            $mes = date('m',strtotime($cobroAlquiler->fecha_cobro));
+            switch ($mes){
+                Case 1:
+                    $mesesPagos[1] = 1;
+                    break;
+                Case 2:
+                    $mesesPagos[2] = 1;
+                    break;
+                Case 3:
+                    $mesesPagos[3] = 1;
+                    break;
+                Case 4:
+                    $mesesPagos[4] = 1;
+                    break;
+                Case 5:
+                    $mesesPagos[5] = 1;
+                    break;
+                Case 6:
+                    $mesesPagos[6] = 1;
+                    break;
+                Case 7:
+                    $mesesPagos[7] = 1;
+                    break;
+                Case 8:
+                    $mesesPagos[8] = 1;
+                    break;
+                Case 9:
+                    $mesesPagos[9] = 1;
+                    break;
+                Case 10:
+                    $mesesPagos[10] = 1;
+                    break;
+                Case 11:
+                    $mesesPagos[11] = 1;
+                    break;
+                Case 12:
+                    $mesesPagos[12] = 1;
+                    break;
+            }
         }
+        return $mesesPagos;
+    }
+
+    /*
+     * Funcion para inicializar los meses en 0
+     */
+    public function initMeses(){
+        for ($i = 1; $i <= 12; $i++ ){
+            $meses[$i] = 0;
+        }
+        return $meses;
     }
 }
