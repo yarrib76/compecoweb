6597ad1938f52e052bd25644a34fe0df106e6ea8
Yamil <yarrib76@gmail.com>
2016-10-04 13:30:38 -0300

    Cambios

--- app/Http/Controllers/Api/ReporteAlquileresAnual.php
+++ app/Http/Controllers/Api/ReporteAlquileresAnual.php
@@ -27,40 +27,40 @@ class ReporteAlquileresAnual extends Controller {
             $mes = date('m',strtotime($cobro_alquiler['fecha_cobro']));
             switch ($mes) {
                 Case 1:
-                    $mesesPagos[1] = ['Enero' => $cobro_alquiler['sum']];
+                    $mesesPagos[1] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 2:
-                    $mesesPagos[2] = ['Febrero' => $cobro_alquiler['sum']];
+                    $mesesPagos[2] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 3:
-                    $mesesPagos[3] = ['Marzo' => $cobro_alquiler['sum']];
+                    $mesesPagos[3] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 4:
-                    $mesesPagos[4] = ['Abril' => $cobro_alquiler['sum']];
+                    $mesesPagos[4] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 5:
-                    $mesesPagos[5] = ['Mayo' => $cobro_alquiler['sum']];
+                    $mesesPagos[5] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 6:
-                    $mesesPagos[6] = ['Junio' => $cobro_alquiler['sum']];
+                    $mesesPagos[6] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 7:
-                    $mesesPagos[7] = ['Julio' => $cobro_alquiler['sum']];
+                    $mesesPagos[7] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 8:
-                    $mesesPagos[8] = ['Agosto' => $cobro_alquiler['sum']];
+                    $mesesPagos[8] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 9:
-                    $mesesPagos[9] = ['Septiempre' => $cobro_alquiler['sum']];
+                    $mesesPagos[9] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 10:
-                    $mesesPagos[10] = ['Octubre' => $cobro_alquiler['sum']];
+                    $mesesPagos[10] = ['Toal' => $cobro_alquiler['sum']];
                     break;
                 Case 11:
-                    $mesesPagos[11] = ['Noviembre' => $cobro_alquiler['sum']];
+                    $mesesPagos[11] = ['Total' => $cobro_alquiler['sum']];
                     break;
                 Case 12:
-                    $mesesPagos[12] = ['Diciembre' => $cobro_alquiler['sum']];
+                    $mesesPagos[12] = ['Total' => $cobro_alquiler['sum']];
                     break;
             }
         }
@@ -69,19 +69,9 @@ class ReporteAlquileresAnual extends Controller {
 
     private function initMesesPagos($mesesPagos)
     {
-        $mesesPagos[1] = ['Enero' => 0];
-        $mesesPagos[2] = ['Febrero' => 0];
-        $mesesPagos[3] = ['Marzo' => 0];
-        $mesesPagos[4] = ['Abril' => 0];
-        $mesesPagos[5] = ['Mayo' => 0];
-        $mesesPagos[6] = ['Junio' => 0];
-        $mesesPagos[7] = ['Julio' => 0];
-        $mesesPagos[8] = ['Agosto' => 0];
-        $mesesPagos[9] = ['Septiembre' => 0];
-        $mesesPagos[10] = ['Octubre' => 0];
-        $mesesPagos[11] = ['Noviembre' => 0];
-        $mesesPagos[12] = ['Diciembre' => 0];
-
+        for ($i = 1; $i <= 12; $i++ ){
+            $mesesPagos[$i] = ['Total' => 0];
+        }
         return $mesesPagos;
     }
 }
