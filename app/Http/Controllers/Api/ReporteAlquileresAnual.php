e955f91ca07de9715105d6f1aab3dbe99f176a9a
Yamil <yarrib76@gmail.com>
2016-10-05 11:59:53 -0300

    Cambios

--- app/Http/Controllers/Api/ReporteAlquileresAnual.php
+++ app/Http/Controllers/Api/ReporteAlquileresAnual.php
@@ -4,8 +4,6 @@ use AlquilerAdmin\Http\Requests;
 use AlquilerAdmin\Http\Controllers\Controller;
 
 use AlquilerAdmin\Models\CobroAlquileres;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
 
 class ReporteAlquileresAnual extends Controller {
@@ -23,47 +21,61 @@ class ReporteAlquileresAnual extends Controller {
     }
 
     private function obtengoSumPorMeses($cobro_alquileres,$mesesPagos){
+        $totalAnual = 0;
         foreach ($cobro_alquileres as $cobro_alquiler){
             $mes = date('m',strtotime($cobro_alquiler['fecha_cobro']));
             switch ($mes) {
                 Case 1:
                     $mesesPagos[1] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 2:
                     $mesesPagos[2] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 3:
                     $mesesPagos[3] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 4:
                     $mesesPagos[4] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 5:
                     $mesesPagos[5] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 6:
                     $mesesPagos[6] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 7:
                     $mesesPagos[7] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 8:
                     $mesesPagos[8] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 9:
                     $mesesPagos[9] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 10:
                     $mesesPagos[10] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 11:
                     $mesesPagos[11] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
                 Case 12:
                     $mesesPagos[12] = ['Total' => $cobro_alquiler['sum']];
+                    $totalAnual = $totalAnual + $cobro_alquiler['sum'];
                     break;
             }
         }
+        $mesesPagos[13] = ['TotalAnual' => $totalAnual];
         return $mesesPagos;
     }
 
