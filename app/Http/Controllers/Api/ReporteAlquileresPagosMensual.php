428e78e6239a6e0e973e63cfe74a847a31c12cd6
Yamil <yarrib76@gmail.com>
2016-11-13 14:48:12 -0300

    Cambios

--- app/Http/Controllers/Api/ReporteAlquileresPagosMensual.php
+++ app/Http/Controllers/Api/ReporteAlquileresPagosMensual.php
@@ -23,6 +23,7 @@ class ReporteAlquileresPagosMensual extends Controller {
 
     private function listaCobrosPorInquiulinos($cobro_alquileres_mensual){
         $x = 1;
+        $inquilinosQuePagaron[] = "";
         foreach ($cobro_alquileres_mensual as $cobro ){
             if ($cobro['alquileres']->estado_alquiler <> 0){
                 $inquilinosQuePagaron[$x] = ['id' => $cobro['alquileres']->load('usuario')->usuario->id,
